e1115b60ff90221025e675e310a3aeaed290f072
yadira16 <yadira.contact@gmail.com>
2016-03-04 10:31:45 -0800

    created namePicker function

--- friday.js
+++ friday.js
@@ -3,3 +3,7 @@ var codetalkNames = [];
 function resetNames() {
 	codetalkNames = ['Bernice', 'Elliette', 'Mirna', 'Erinn', "Aimee", 'Yadira', 'Jessica', 'Janene', 'Denise']
 }
+
+function namePicker() {
+	var namePick = codetalkNames.splice(Math.floor(Math.random() * codetalkNames.length), 1);
+}
